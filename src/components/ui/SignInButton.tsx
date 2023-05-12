e3dbf0ba363fee2a96ea1ccad554361525d64e88
Leo Goubard <leogoubard@macbook-pro-de-leo.home>
2023-05-12 22:48:06 +0200

    :construction: setup already done, building some custom components in progress

---
+++ src/components/ui/SignInButton.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { FC, useState } from 'react';
+import Button from "@/components/ui/Button";
+import { signIn } from 'next-auth/react';
+import toast from "react-hot-toast";
+
+interface SignInButtonProps {
+  
+}
+
+const SignInButton: FC<SignInButtonProps> = ({}) => {
+    const [isLoading, setIsloading] = useState<boolean>(false)
+
+    const signInWithGoogle = async() => {
+        setIsloading(true)
+
+        try {
+            await signIn('google')
+        } catch (error) {
+            toast.error('Please try again later.')
+        }
+    }
+
+    return (
+        <Button onClick={signInWithGoogle} isLoading={isLoading}>
+
+        </Button>
+    )
+}
+
+export default SignInButton
